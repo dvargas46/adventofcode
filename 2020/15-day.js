f212d65100a65a9c0e06485fab20953e2300a51e
Vargas, Dan <dan.vargas@frostbank.com>
2020-12-15 05:32:08 -0600

    add 2020-15

---
+++ 2020/15-day.js
@@ -0,0 +1,51 @@
+const inputfile = './inputs/15-input.txt';
+// const inputfile = './inputs/15-example.txt';
+const processFile = require('./file-processor');
+let counter = 1;
+
+// PART I
+const input1 = processFile(inputfile, {
+    lineSeparator: ',',
+});
+
+let memory = input1.reduce((mem, val) => (mem[val]=[counter++], mem), {});
+let lastNumberSpoken = input1[input1.length-1];
+
+const addToMemory = (num) => {
+    if (!memory[num]) {
+        memory[num] = [counter++];
+    } else if (memory[num].length === 1) {
+        memory[num].push(counter++);
+    } else {
+        memory[num].shift();
+        memory[num].push(counter++);
+    }
+}
+
+const runTurns = (MAX) => {
+    while (counter <= MAX) {
+        if(counter % 1000000 === 0 ) console.log(counter);
+        if (memory[lastNumberSpoken].length === 1) {
+            lastNumberSpoken = 0;
+            addToMemory(0);
+        } else if (memory[lastNumberSpoken].length === 2) {
+            turns = memory[lastNumberSpoken];
+            diff = turns[1] - turns[0];
+            lastNumberSpoken = diff;
+            addToMemory(diff);
+        }
+    }
+}
+runTurns(2020);
+
+// console.log(memory);
+console.log(counter-1, lastNumberSpoken);
+
+// PART II
+memory = input1.reduce((mem, val) => (mem[val]=[counter++], mem), {});
+let lastNumberSpoken = input1[input1.length-1];
+runTurns(30000000);
+
+// DON'T PRINT THE MEMORY FOR PART II --- YOU WILL REGRET IT
+// console.log(memory);
+console.log(counter-1, lastNumberSpoken);
