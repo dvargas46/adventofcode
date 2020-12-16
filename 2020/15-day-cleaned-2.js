df02b8b52ab9a3dac321488f97869fa817525ee8
Dan Vargas <danvargas46@gmail.com>
2020-12-16 10:46:23 -0600

    cleaned 2020-15

---
+++ 2020/15-day-cleaned-2.js
@@ -0,0 +1,46 @@
+const inputfile = './inputs/15-input.txt';
+// const inputfile = './inputs/15-example.txt';
+const processFile = require('./file-processor');
+let counter = 1;
+
+// PART I
+const input1 = processFile(inputfile, {
+    lineSeparator: ',',
+});
+
+let memory1 = input1.reduce((mem, val) => (mem.set(parseInt(val), counter++), mem), new Map());
+let memory2 = new Map();
+let lastNumberSpoken = parseInt(input1[input1.length-1]);
+
+const addToMemory = (num) => {
+    if (!memory1.has(num)) {
+        memory1.set(num, counter++);
+    } else if (!memory2.has(num)) {
+        memory2.set(num, counter++);
+    } else {
+        memory1.set(num, memory2.get(num));
+        memory2.set(num, counter++);
+    }
+}
+
+const runTurns = (MAX) => {
+    while (counter <= MAX) {
+        if (!memory2.has(lastNumberSpoken)) {
+            lastNumberSpoken = 0;
+            addToMemory(0);
+        } else {
+            lastNumberSpoken = memory2.get(lastNumberSpoken) - memory1.get(lastNumberSpoken);
+            addToMemory(lastNumberSpoken);
+        }
+    }
+}
+
+// PART I
+// runTurns(2020);
+
+// PART II
+runTurns(30000000);
+
+// DON'T PRINT THE MEMORY FOR PART II --- YOU WILL REGRET IT
+// console.log(memory);
+console.log(counter-1, lastNumberSpoken);
--- 2020/15-day-cleaned.js
+++ 2020/15-day-cleaned.js
@@ -6,28 +6,27 @@ let counter = 1;
 // PART I
 const input1 = processFile(inputfile, { lineSeparator: ',' });
 
-const memory = input1.reduce((mem, val) => (mem[parseInt(val)]=[counter++], mem), {});
+const memory = input1.reduce((mem, val) => (mem.set(parseInt(val),[counter++]), mem), new Map());
 let lastNumberSpoken = parseInt(input1[input1.length-1]);
 
 const addToMemory = (num) => {
-    if (!(num in memory)) {
-        memory[num] = [counter++];
-    } else if (memory[num].length === 1) {
-        memory[num].push(counter++);
+    if (!memory.has(num)) {
+        memory.set(num, [counter++]);
+    } else if (memory.get(num).length === 1) {
+        memory.get(num).push(counter++);
     } else {
-        memory[num].shift();
-        memory[num].push(counter++);
+        memory.get(num).shift();
+        memory.get(num).push(counter++);
     }
 }
 
 const runTurns = (MAX) => {
     while (counter <= MAX) {
-        if(counter % 1000000 === 0 ) console.log(counter);
-        if (memory[lastNumberSpoken].length === 1) {
+        if (memory.get(lastNumberSpoken).length === 1) {
             lastNumberSpoken = 0;
             addToMemory(0);
-        } else if (memory[lastNumberSpoken].length === 2) {
-            turns = memory[lastNumberSpoken];
+        } else if (memory.get(lastNumberSpoken).length === 2) {
+            turns = memory.get(lastNumberSpoken);
             diff = turns[1] - turns[0];
             lastNumberSpoken = diff;
             addToMemory(diff);
