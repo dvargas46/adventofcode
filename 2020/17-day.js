1001590ab9789e6bb4d60c46ae04be9cf29748d0
Dan Vargas <danvargas46@gmail.com>
2020-12-17 08:24:15 -0600

    start 2020-17

---
+++ 2020/17-day.js
@@ -0,0 +1,39 @@
+const inputfile = './inputs/17-input.txt';
+// const inputfile = './inputs/17-example.txt';
+const processFile = require('./file-processor');
+
+
+// PART I
+const input1 = processFile(inputfile, {
+    lineSeparator: new RegExp('\\n', 'g'),
+    // groupSeparator: new RegExp('\\s', 'g'),
+    // jsonSeparator: new RegExp(':', 'g')
+});
+
+const result1 = input1;
+console.log(result1);
+
+
+
+// PART II
+// const result2 = input1;
+// console.log(result2);
+
+// z=-1
+// .....
+// .....
+// .....
+// .....
+// .....
+// z=0
+// .....
+// ..#..
+// ...#.
+// .###.
+// .....
+// z=1
+// .....
+// .....
+// .....
+// .....
+// .....
